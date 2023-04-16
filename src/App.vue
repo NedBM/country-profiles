c4eeb0e4572afd337c5acc4b2308e37bc319a291
neomarino333 <61431560+neomarino333@users.noreply.github.com>
2023-04-15 22:21:26 -0400

    card layout mock

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import CountryProfileComponent from './components/CountryProfileComponent.vue';
+
+const mockCountry = {
+  name: 'United States',
+  region: 'North America',
+  description:
+    'The United States of America is a country primarily located in North America. It consists of 50 states, a federal district, five major self-governing territories, 326 Indian reservations, and some minor possessions.',
+};
 </script>
 
-<template>
+<!-- <template>
   <div>
     <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+      Hello world!
+    </h1>
+    <CountryProfileComponent :country="mockCountry" />
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
@@ -15,6 +24,29 @@ import HelloWorld from './components/HelloWorld.vue'
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
+</template> -->
+
+<template>
+  <div class="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+    <div class="container mx-auto px-4">
+      <h1 class="text-3xl font-bold underline mb-8">
+        Hello world!
+      </h1>
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <CountryProfileComponent :country="mockCountry" />
+        <CountryProfileComponent :country="mockCountry" />
+        <CountryProfileComponent :country="mockCountry" />
+        <CountryProfileComponent :country="mockCountry" />
+        <CountryProfileComponent :country="mockCountry" />
+        <CountryProfileComponent :country="mockCountry" />
+        <CountryProfileComponent :country="mockCountry" />
+        <CountryProfileComponent :country="mockCountry" />
+        <CountryProfileComponent :country="mockCountry" />
+        <CountryProfileComponent :country="mockCountry" />
+        <!-- Add more CountryCard components as needed -->
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
