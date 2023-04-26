dfc6fcc68b066ad884e2d7749b5254c729338970
neomarino333 <61431560+neomarino333@users.noreply.github.com>
2023-04-26 03:35:46 -0400

    fix post css

--- src/components/CountryDetailComponent.vue
+++ src/components/CountryDetailComponent.vue
@@ -2,11 +2,12 @@
   <div>
     <h1 v-if="country">{{ country.name }} Details</h1>
     <div v-else>Loading...</div>
-    <!-- Add more information and D3js visualizations here -->
+    <div id="chart"></div>
   </div>
 </template>
 
 <script setup lang="ts">
+import * as d3 from 'd3';
 interface Country {
   id: string;
   name: string;
@@ -23,8 +24,31 @@ onMounted(async () => {
   try {
     const response = await axios.get(`https://api.worldbank.org/v2/country/${route.params.id}?format=json`);
     country.value = response.data[1][0];
+    
+    // Call drawChart() after fetching data
+    drawChart();
   } catch (error) {
     console.error('Error fetching country:', error);
   }
 });
+
+function drawChart() {
+  // Use the D3.js API to create your chart
+  // Example: create a simple bar chart with random data
+  const data = [30, 86, 168, 281, 303, 365];
+  const svg = d3.select("#chart")
+    .append("svg")
+    .attr("width", 500)
+    .attr("height", 150);
+
+  svg.selectAll("rect")
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr("x", (d, i) => i * 25)
+    .attr("y", (d) => 150 - d)
+    .attr("width", 20)
+    .attr("height", (d) => d)
+    .attr("fill", "blue");
+}
 </script>
