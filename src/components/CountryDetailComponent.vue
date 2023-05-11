70b1c23eb9f172f27c8708c21872fa8011079718
neomarino333 <61431560+neomarino333@users.noreply.github.com>
2023-05-10 23:59:29 -0400

    add: label test

--- src/components/CountryDetailComponent.vue
+++ src/components/CountryDetailComponent.vue
@@ -212,6 +212,7 @@ svg
 
     </div>
     <div class="stat-title">Population</div>
+    <div class="stat-title">Population</div>
   <div class="stat-value animate-fade-up">{{ stats.population ? `${numberWithCommas(stats.population)}` : 'N/A' }}</div>
   <div class="stat-desc animate-fade-up">2020</div>
   </div>
@@ -222,7 +223,10 @@ svg
   <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v12m-3-2.818l.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
 </svg>
 
+
     </div>
+    
+    
     <div class="stat-title">GDP per capita</div>
   <div class="stat-value animate-fade-up">{{ stats.gdpPerCapita ? `$${formatGDP(stats.gdpPerCapita)}` : 'N/A' }}</div>
   <div class="stat-desc animate-fade-up">2020</div>
